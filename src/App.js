6896b383afabd77a93b44fc644d132108051042d
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2021-07-09 15:42:09 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+//Pages
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+//components
+import NavBar from './components/layout/NavBar';
+//Routing
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddUser from './components/pages/users/AddUser';
+import EditUser from './components/pages/users/EditUser';
+import ViewUser from './components/pages/users/ViewUser';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/add" component={AddUser} /> 
+          <Route exact path="/users/edit/:id" component={EditUser} />  
+          <Route exact path="/users/:id" component={ViewUser} />  
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
